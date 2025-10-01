50074a66af284d9d5d7a257c47d878ac30255fef
BruhKrypticc <syncoperating@gmail.com>
2025-10-01 19:30:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-/* Reset default margin and padding */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-/* Set default fonts and background for the whole app */
-html, body, #root {
-  height: 100%;
-  font-family: 'Inter', sans-serif;
-  background: black;
-  color: white;
-  overflow-x: hidden;
-  font-weight: 800;
-}
-
-/* Smooth scrolling for anchor links or scrolling effects */
-html {
-  scroll-behavior: smooth;
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
